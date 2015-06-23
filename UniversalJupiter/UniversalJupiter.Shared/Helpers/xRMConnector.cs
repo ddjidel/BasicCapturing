bd5ed15530e6c1ae698e5a5d9532d9fd759f0eb1
Dalil Djidel <ddjidel@outlook.com>
2015-06-23 08:58:10 +0200

    init

---
+++ UniversalJupiter/UniversalJupiter.Shared/Helpers/xRMConnector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UniversalJupiter.Helpers
+{
+    public static class xRMConnector
+    {
+        public class Lead
+        {
+            public string Subject { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string NoteSubject { get; set; }
+            public string FileName { get; set; }
+            public string MimeType { get; set; }
+            public byte[] Data { get; set; }
+        }
+        public async static void CreateLead(string subject, string firstName, string lastName, string noteSubject, string fileName, string mimeType, byte[] data)
+        {
+            JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+            Lead lead = new Lead();
+            lead.Subject = subject;
+            lead.FirstName = firstName;
+            lead.LastName = lastName;
+            lead.NoteSubject = noteSubject;
+            lead.FileName = fileName;
+            lead.MimeType = mimeType;
+            lead.Data = data;
+
+            var json = JsonConvert.SerializeObject(lead);
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+            try
+            {
+                var uri = "http://xrmconnector.azurewebsites.net/api/Lead";
+                HttpClient request = new HttpClient();
+                request.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await request.PostAsync(uri, new StringContent(json, Encoding.UTF8, "application/json"));
+            }
+            catch (HttpRequestException hre)
+            {
+                
+            }
+        }
+    }
+}
---
+++ UniversalJupiter/UniversalJupiter.Shared/PhotoCapturePage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using UniversalJupiter.Helpers;
+using Windows.Storage.Streams;
+
+namespace UniversalJupiter
+{
+    public sealed partial class PhotoCapturePage
+    {
+        private CameraCapture cameraCapture;
+
+        public PhotoCapturePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Init and show preview
+            cameraCapture = new CameraCapture();
+            PreviewElement.Source = await cameraCapture.Initialize();
+            await cameraCapture.StartPreview();
+        }
+
+        protected override async void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Release resources
+            if (cameraCapture != null)
+            {
+                await cameraCapture.StopPreview();
+                PreviewElement.Source = null;
+                cameraCapture.Dispose();
+                cameraCapture = null;
+            }
+        }
+
+        private async void BtnCapturePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            // Take snapshot and add to ListView
+            // Disable button to prevent exception due to parallel capture usage
+            BtnCapturePhoto.IsEnabled = false;
+            var photoStorageFile = await cameraCapture.CapturePhoto();
+
+            byte[] fileBytes = null;
+
+            using (IRandomAccessStreamWithContentType stream = await photoStorageFile.OpenReadAsync())
+            {
+                fileBytes = new byte[stream.Size];
+
+                using (DataReader reader = new DataReader(stream))
+                {
+                    await reader.LoadAsync((uint)stream.Size);
+                    reader.ReadBytes(fileBytes);
+                }
+            }
+
+            var bitmap = new BitmapImage();
+            await bitmap.SetSourceAsync(await photoStorageFile.OpenReadAsync());
+            PhotoListView.Items.Add(bitmap);
+            BtnCapturePhoto.IsEnabled = true;
+
+            xRMConnector.CreateLead("Candidate", "Laetitia", "Casta", "CV", "CV.jpeg", "image/jpeg", fileBytes);
+        }
+    }
+}
